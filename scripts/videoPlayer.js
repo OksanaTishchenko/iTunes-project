35f1b760ffdc60a5a8a03ec7a37681442c78f2af
OksanaTishchenko <oksana.tishchenko@ukr.net>
2021-01-17 13:02:32 +0200

    add project

---
+++ scripts/videoPlayer.js
@@ -0,0 +1,90 @@
+import { addZero } from './supScript.js';
+
+export const videoPlayerInit = () => {
+  console.log('Video init');
+
+  const videoPlayer = document.querySelector('.video-player');
+  const videoButtonPlay = document.querySelector('.video-button__play');
+  const videoButtonStop = document.querySelector('.video-button__stop');
+  const videoProgress = document.querySelector('.video-progress');
+  const videoTimePassed = document.querySelector('.video-time__passed');
+  const videoTimeTotal = document.querySelector('.video-time__total');
+  const videoVolume = document.querySelector('.video-volume');
+  const videoFullscreen = document.querySelector('.video-fullscreen');
+
+  videoFullscreen.addEventListener('click', () => {
+    videoPlayer.requestFullscreen(); // Встроенный МЕТОД видеоплееров
+  })
+
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove('fa-pause');
+      videoButtonPlay.classList.add('fa-play');
+    } else {
+      videoButtonPlay.classList.add('fa-pause');
+      videoButtonPlay.classList.remove('fa-play');
+    }
+  }
+
+  const togglePlay = (event) => {
+    event.preventDefault();
+    if (videoPlayer.paused) {
+      videoPlayer.play();
+    } else {
+      videoPlayer.pause();
+    }
+  };
+
+  const stopPlay = () => {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+  }
+
+  const changeValue = () => {
+    const valueVolum = videoVolume.value;
+    videoPlayer.volume = valueVolum / 100;
+  };
+
+  videoPlayer.addEventListener('click', togglePlay);
+  videoButtonPlay.addEventListener('click', togglePlay);
+
+
+  videoPlayer.addEventListener('play', toggleIcon);
+  videoPlayer.addEventListener('pause', toggleIcon);
+
+  videoButtonStop.addEventListener('click', stopPlay);
+
+  videoPlayer.addEventListener('timeupdate', () => {
+    const currentTime = videoPlayer.currentTime;
+    const duration = videoPlayer.duration;
+
+    videoProgress.value = (currentTime / duration) * 100;
+
+    let minutePassed = Math.floor(currentTime / 60);
+    let secondsPassed = Math.floor(currentTime % 60);
+
+    let minuteTotal = Math.floor(duration / 60);
+    let secondsTotal = Math.floor(duration % 60);
+
+    videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
+    videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`;
+  });
+
+  videoProgress.addEventListener('change', () => {
+    const duration = videoPlayer.duration;
+    const value = videoProgress.value;
+
+    console.log(duration, value);
+
+    videoPlayer.currentTime = (value * duration) / 100;
+    console.log(videoPlayer.currentTime);
+  });
+
+  videoVolume.addEventListener('input', changeValue);
+  changeValue();
+
+  return () => {
+    videoPlayer.pause();
+    toggleIcon();
+  }
+};
